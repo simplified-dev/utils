6e5efe6f48f0e6445f0e1d7be82390772ba8e36f
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2026-05-15 00:45:07 -0400

    Pin collections strictly to 6586657

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ repositories {
 
 dependencies {
     // Simplified Collections
-    api("com.github.simplified-dev:collections") { version { strictly("a5f41e0") } }
+    api("com.github.simplified-dev:collections") { version { strictly("6586657") } }
 
     // JetBrains Annotations
     api(libs.annotations)
